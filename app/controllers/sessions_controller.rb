dfdcaacc511b9bdcf11577ca8870f27c913b9d54
An Tran Minh <tranminhan.my@gmail.com>
2015-12-10 19:50:09 +0700

    flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      debugger
+      flash[:success] = "Welcome to Tinder, #{user.name}!"
       redirect_to users_path
     else 
-      # flash[:danger] = ""
+      flash[:warning] = "Username and password are incorrect"
       render 'new'
     end 
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:success] = "See you again!"
     redirect_to root_path
   end 
 end
