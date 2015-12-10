18e4b09a1cd83d643fea53841b8cf65ad39e100b
An Tran Minh <tranminhan.my@gmail.com>
2015-12-10 19:29:33 +0700

    signup and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,24 @@ class UsersController < ApplicationController
   def index
     @users = User.all.shuffle
   end
+
+  def show
+  
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else 
+      render 'new'
+    end 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  
   root 'welcome#index'
   get 'welcome/index'
 
   resources :users
+  resources :sessions
+
+  get 'signup' => 'users#new'
+  get 'signin' => 'sessions#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
